8c32d2936f733eb73285ed039f1d7ee46398dfab
Rafa <khanrafa810@gmail.com>
2024-03-10 12:44:28 +0530

    final commit

--- src/main/scala/EmployeeAnalyzer.scala
+++ src/main/scala/EmployeeAnalyzer.scala
@@ -1,11 +1,15 @@
 import scala.io.Source
 import Employee.createFromCsv
-import scala.util.{Try, Success, Failure}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
+//import scala.concurrent.duration._
 
 
 object EmployeeAnalyzer extends App {
 
-  val filePath = "C:\\Users\\c22832b\\Downloads\\Employee.csv"
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
+  private val filePath = "C:\\Users\\c22832b\\Downloads\\Employee.csv"
   val employeeList = loadData(filePath)
   loadData(filePath) match {
     case Success(employeeList) =>
@@ -15,9 +19,23 @@ object EmployeeAnalyzer extends App {
       println("----------------------------")
       println("|          REPORT          |")
       println("----------------------------")
-      calculateStatistics(preprocessedEmployeeList)
-      analyzeRetentionFactors(preprocessedEmployeeList)
-      segmentEmployees(preprocessedEmployeeList)
+
+      val statisticsFuture: Future[List[Any]] = calculateStatistics(preprocessedEmployeeList)
+      val retentionFactorsFuture: Future[List[Any]] = analyzeRetentionFactors(preprocessedEmployeeList)
+      val segmentationFuture: Future[List[Any]] = segmentEmployees(preprocessedEmployeeList)
+
+      // Await.result(awaitable = statisticsFuture, atMost = 10.seconds)
+      // println("---------------11111111111111111" )
+
+
+      //Combining the futures into a single future for easier handling, waiting for all the futures to complete execution
+      Future.sequence(Seq(statisticsFuture, retentionFactorsFuture, segmentationFuture)).onComplete {
+        case Success(_) => generateReport(statisticsFuture,retentionFactorsFuture,segmentationFuture)
+
+        case Failure(exception) =>
+          println(s"Error during concurrent processing: ${exception.getMessage}")
+      }
+      Thread.sleep(5000)
 
 
     case Failure(exception) =>
@@ -25,7 +43,7 @@ object EmployeeAnalyzer extends App {
   }
 
   // Function to load data from a CSV file
-  def loadData(filePath: String): Try[List[Employee]] = Try{
+  private def loadData(filePath: String): Try[List[Employee]] = Try{
     val bufferedSource = Source.fromFile(filePath) // Open the file for reading
     val lines = bufferedSource.getLines().drop(1).toList // Read lines, skip the header, and convert to a list
     bufferedSource.close()
@@ -35,92 +53,65 @@ object EmployeeAnalyzer extends App {
 
 
   //Function to handle missing data
-      def handleMissingData(employee: Employee): Employee = {
-        val updatedPaymentTier = if (employee.paymentTier < 0) 0 else employee.paymentTier
-        employee.copy(paymentTier = updatedPaymentTier)
-      }
+  private def handleMissingData(employee: Employee): Employee = {
+    val updatedPaymentTier = if (employee.paymentTier < 0) 0 else employee.paymentTier
+    employee.copy(paymentTier = updatedPaymentTier)
+  }
 
-      // Function to preprocess data
-      def preprocessData(employeeList: List[Employee]): List[Employee] = {
-        employeeList.map(handleMissingData)
-          .filter(_.age >= 0)
-      }
+  // Function to preprocess data
+  private def preprocessData(employeeList: List[Employee]): List[Employee] = {
+    employeeList.map(handleMissingData)
+      .filter(_.age >= 0)
+  }
 
-      //Functions to calculate statistics:
-      def calculateStatistics(employeeList: List[Employee]): Unit = {
+  //Functions to calculate statistics:
+  private def calculateStatistics(employeeList: List[Employee]): Future[List[Any]] = Future{
+    //Average age:
+    val averageAge = employeeList.map(_.age).sum.toDouble / employeeList.length
+    //Gender Count:
+    val maleCount = employeeList.count(_.gender.equalsIgnoreCase("Male"))
+    val femaleCount = employeeList.length - maleCount
+    //Education Level:
+    val educationLevelDistribution = employeeList.groupBy(_.educationLevel).view.mapValues(_.size).toList
 
-        println("----------------------------")
-        println("Statistical Analysis :")
-        println("----------------------------")
+    List(averageAge,maleCount,femaleCount,educationLevelDistribution)
+  }
 
-        //Average age:
-        val averageAge = employeeList.map(_.age).sum.toDouble / employeeList.length
-        println(s"Average Age:$averageAge")
 
-        //Gender Count:
-        val maleCount = employeeList.count(_.gender.equalsIgnoreCase("Male"))
-        val femaleCount = employeeList.length - maleCount
-        println(s"Gender Ratio (Male/Female): $maleCount/$femaleCount")
+  //Function to analyze factors affecting employee retention
+  private def analyzeRetentionFactors(employeeList: List[Employee]): Future[List[Any]] = Future{
+    //Payment Tier analysis:
+    val paymentTierDistribution = employeeList.groupBy(_.paymentTier).view.mapValues(_.size).toList
+    //Bench Status Analysis:
+    val benchStatusDistribution = employeeList.groupBy(_.benchStatus).view.mapValues(_.size).toList
+    //Experience in current domain analysis:
+    val experienceDistribution = employeeList.groupBy(_.experienceInCurrentDomain).view.mapValues(_.size).toList
 
-        //Education Level:
-        val educationLevelDistribution = employeeList.groupBy(_.educationLevel).view.mapValues(_.size)
-        println("Education Level Distribution:")
-        educationLevelDistribution.foreach {
-          case (level, count) => println(s"$level : $count")
-        }
+    List(paymentTierDistribution, benchStatusDistribution, experienceDistribution)
+  }
 
-      }
+  //Functions to segment employees based on attributes:
+  private def segmentEmployees(employeeList: List[Employee]): Future[List[Any]] = Future{
+    //Segment Employees based on city
+    val citySegmentation = employeeList.groupBy(_.city).view.mapValues(_.size).toList
+    //Segment Employees based on Education Level:
+    val educationSegmentation = employeeList.groupBy(_.educationLevel).view.mapValues(_.size).toList
 
-      //Function to analyze factors affecting employee retention
-      def analyzeRetentionFactors(employeeList: List[Employee]): Unit = {
+    List(citySegmentation, educationSegmentation)
+  }
 
-        println("----------------------------")
-        println("Retention Factor Analysis :")
-        println("----------------------------")
-
-        //Payment Tier analysis:
-        val paymentTierDistribution = employeeList.groupBy(_.paymentTier).view.mapValues(_.size)
-        println("Payenment Tier Analysis:")
-        paymentTierDistribution.foreach {
-          case (tier, count) => println(s"Payment Tier $tier: $count")
-        }
-
-        //Bench Status Analysis:
-        val benchStatusDistribution = employeeList.groupBy(_.benchStatus).view.mapValues(_.size)
-        println("Bench Status Analysis :")
-        benchStatusDistribution.foreach {
-          case (status, count) => println(s"Bench Status $status : $count")
-        }
-
-        //Experience in current domain analysis:
-        val experienceDistribution = employeeList.groupBy(_.experienceInCurrentDomain).view.mapValues(_.size)
-        println("Experience In Current Domain Analysis:")
-        experienceDistribution.foreach {
-          case (exp, count) => println(s"Experience in Current Domain - $exp : $count")
-        }
-      }
-
-      //Functions to segment employees based on attributes:
-      def segmentEmployees(employeeList: List[Employee]): Unit = {
-
-        println("----------------------------")
-        println("Segmentation Analysis :")
-        println("----------------------------")
-
-        //Segment Employees based on city
-        val citySegmentation = employeeList.groupBy(_.city).view.mapValues(_.size)
-        println("Segmentation Analysis: (City Wise)")
-        citySegmentation.foreach {
-          case (city, count) => println(s"$city : $count")
-        }
-
-        //Segment Employees based on Education Level:
-        val educationSegmentation = employeeList.groupBy(_.educationLevel)
-        println("Segmentation Analysis: (Education Level wise:)")
-        educationSegmentation.foreach {
-          case (level, employees) =>
-            val educationlevelCount = employees.size
-            println(s"$level : $educationlevelCount")
-        }
-      }
+  def generateReport(statistical : Future[List[Any]], retention : Future[List[Any]], segmentation: Future[List[Any]]): Unit = {
+    println("----------------------------")
+    println("Segmentation Analysis :")
+    println("----------------------------")
+    println(statistical.toString)
+    println("----------------------------")
+    println("Retention Factor Analysis :")
+    println("----------------------------")
+    println(retention.toString)
+    println("----------------------------")
+    println("Segmentation Analysis :")
+    println("----------------------------")
+    println(segmentation.toString)
+  }
 }
